4779cb01060faf98df44f174ccf1ccc4c2dce6ab
michael <michael99940@gmail.com>
2019-05-12 23:35:54 -0700

    first commit

---
+++ Client/App.js
@@ -0,0 +1,118 @@
+var ticTacToeApp = angular.module('ticTacToeApp', []);
+
+
+ticTacToeApp.controller('ticTacToeController', function ($scope) {
+  $scope.board = [[{value: '-'}, {value: '-'}, {value: '-'}],[{value: '-'}, {value: '-'}, {value: '-'}],[{value: '-'}, {value: '-'}, {value: '-'}]];
+  $scope.player = false;
+  $scope.win = false;
+  $scope.tie = false;
+  $scope.message = ' ';
+  $scope.resetClass = 'no-highLight';
+  $scope.entryClass = [['no-highLight','no-highLight','no-highLight'],['no-highLight','no-highLight','no-highLight'],['no-highLight','no-highLight','no-highLight']];
+
+  $scope.reset = function() {
+    for (let i = 0; i < $scope.board.length; i++) {
+      for (let j = 0; j < $scope.board[i].length; j++) {
+        $scope.board[i][j].value = '-';
+      }
+    }
+
+    $scope.player = false;
+    $scope.winner = null;
+    $scope.tie = false;
+    $scope.message = ' ';
+    $scope.resetClass = 'no-highLight';
+    $scope.entryClass = [['no-highLight','no-highLight','no-highLight'],['no-highLight','no-highLight','no-highLight'],['no-highLight','no-highLight','no-highLight']];
+  }
+
+  $scope.showPlayer = function() {
+    if($scope.message.length > 1) {
+      return $scope.message;
+    }else if($scope.player) {
+      return "Current Player : 'X'";
+    } else {
+      return "Current Player : 'O'";;
+    }
+  }
+
+  $scope.filled = function(entry,i,j) {
+    if($scope.winner || $scope.tie) {
+      $scope.entryClass[i][j] = 'no-highLight';
+      return true;
+    } else if (entry.value !== '-') {
+      $scope.entryClass[i][j] = 'no-highLight';
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  $scope.checkWin = function() {
+    var tie = true;
+    for (let i = 0; i < $scope.board.length; i++) {
+      for (let j = 0; j < $scope.board[i].length; j++) {
+        if($scope.board[i][j].value === '-') {
+          tie = false;
+        }
+        if (($scope.board[i][0].value === $scope.board[i][1].value) && ($scope.board[i][0].value === $scope.board[i][2].value && ($scope.board[i][0].value !== '-'))) {
+          $scope.winner = $scope.board[i][0].value;
+          return;
+        }
+        if (($scope.board[0][j].value === $scope.board[1][j].value) && ($scope.board[0][j].value === $scope.board[2][j].value && ($scope.board[0][j].value !== '-'))) {
+          $scope.winner = $scope.board[0][j].value;
+          return;
+        }
+      }
+    }
+    if (($scope.board[0][0].value === $scope.board[1][1].value) && ($scope.board[0][0].value === $scope.board[2][2].value && ($scope.board[0][0].value !== '-'))) {
+      $scope.winner = $scope.board[0][0].value;
+      return;
+    }
+    if (($scope.board[0][2].value === $scope.board[1][1].value) && ($scope.board[0][2].value === $scope.board[2][0].value && ($scope.board[0][2].value !== '-'))) {
+      $scope.winner = $scope.board[0][2].value;
+      return;
+    }
+    if (tie) {
+      $scope.tie = true;
+      return;
+    }
+  }
+
+  $scope.makeTurn = function(i,j) {
+    if($scope.board[i][j].value !== '-') {
+      return;
+    } else {
+      if($scope.player) {
+        $scope.board[i][j].value = 'X'; 
+        $scope.player = !$scope.player;
+      } else {
+        $scope.board[i][j].value = 'O';
+        $scope.player = !$scope.player;
+      }
+      $scope.checkWin();
+      if ($scope.winner) {
+        $scope.message = "Player " +  $scope.winner + " has won!";
+        return;
+      } else if ($scope.tie) {
+        $scope.message = 'The game has reached a stalemate! Please reset.';
+        return;
+      }
+    }
+  }
+
+  $scope.onEvent = function () {
+    $scope.resetClass = 'highLight';
+  }
+
+  $scope.offEvent = function () {
+    $scope.resetClass = 'no-highLight';
+  }
+  
+  $scope.onEntryEvent = function(i,j) {
+    $scope.entryClass[i][j] = 'highLight';
+  }
+
+  $scope.offEntryEvent = function(i,j) {
+    $scope.entryClass[i][j] = 'no-highLight';
+  }
+});
---
+++ README.md
@@ -0,0 +1,12 @@
+#Angular TicTacToe assignment for Michael Lau
+
+#To launch the Application, either:
+
+    enter npm start in the command line
+
+    browse to localhost:8000 on google chrome
+
+
+#OR
+
+    open index.html on google chrome
